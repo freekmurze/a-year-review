c5ac0eb0f11f07fb1ad08a9b9b93ab2442bbe1a5
Dieter Stinglhamber <d.stinglhamber@altavia-act.com>
2018-12-31 06:14:40 +0100

    (feat): add matomo tracking code

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -6,7 +6,7 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="{{ mix('css/app.css', 'assets/build') }}">
 
-        <title>A yearly review</title>
+        <title>A year review</title>
         <meta name="description" content="Review about the past year" />
 
         <meta property="og:site_name" content="A yearly review">
@@ -20,5 +20,21 @@
         <main class="container mx-auto mt-16">
             @yield('body')
         </main>
+
+        <script type="text/javascript">
+            var _paq = _paq || [];
+
+            _paq.push(["disableCookies"]);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+
+            (function() {
+                var u="//analytics.elhebert.be/";
+                _paq.push(['setTrackerUrl', u+'piwik.php']);
+                _paq.push(['setSiteId', '2']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+            })();
+        </script>
     </body>
 </html>
