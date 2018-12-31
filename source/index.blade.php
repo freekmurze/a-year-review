666c91c9c34bab9d17cd416c472985493e7aa3a1
Dieter Stinglhamber <d.stinglhamber@altavia-act.com>
2018-12-31 06:46:34 +0100

    (feat): improve update copy

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,8 +3,9 @@
 @section('body')
 <h1 class="text-default text-5xl tracking-wide my-8 font-thin">2018's reviews</h1>
 
-<p class="my-8">
-    To add a link to your review update the <a href="https://github.com/Elhebert/a-yearly-review/blob/master/config.php"><code>config.php</code></a> file and make a PR.
+<p class="my-8 border-l-4 border-blue pl-4">
+    <strong>Is someone's retrospective missing?</strong><br>
+    To add a link to the list, update the <a class="text-blue-dark hover:no-underline" href="https://github.com/Elhebert/a-yearly-review/blob/master/config.php"><code>config.php</code></a> file and make a PR.
 </p>
 
 <ul class="list-reset">
